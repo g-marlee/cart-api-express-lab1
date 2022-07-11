9e6c564abc1f194ace735865bfd78713b1404499
Marlee Garcia <marlee.garcia121@gmail.com>
2022-07-11 18:49:08 -0400

    Fixed variable redeclaration

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -6,35 +6,26 @@ import { v4 as uuidv4 } from "uuid";
 export const cartRouter = express.Router();
 
 cartRouter.get('/', (req, res) => {
+    const maxPrice = Number(req.query.maxPrice);
+    const prefix = String(req.query.prefix);
+    const pageSize = Number(req.query.pageSize);
+
     if (req.query.maxPrice && !req.query.prefix && !req.query.pageSize) {
-        const maxPrice = Number(req.query.maxPrice);
         res.json(cart.filter(item => item.price <= maxPrice));        
     } else if (req.query.maxPrice && req.query.prefix && !req.query.pageSize) {
-        const maxPrice = Number(req.query.maxPrice);
-        const prefix = String(req.query.prefix);
         const newCart = cart.filter(item => item.product.startsWith(prefix));
         res.json(newCart.filter(item => item.price <= maxPrice));
     } else if (req.query.maxPrice && req.query.pageSize && !req.query.prefix) {
-        const maxPrice = Number(req.query.maxPrice);
-        const pageSize = Number(req.query.pageSize);
         const newCart = cart.filter(item => item.price <= maxPrice);
         res.json(newCart.slice(0, pageSize));
     } else if (req.query.prefix && !req.query.maxPrice && !req.query.pageSize) {
-        const prefix = String(req.query.prefix);
         res.json(cart.filter(item => item.product.startsWith(prefix)));        
     } else if (req.query.prefix && req.query.pageSize && !req.query.maxPrice) {
-        const prefix = String(req.query.prefix);
-        const pageSize = Number(req.query.pageSize);
         const newCart = cart.filter(item => item.product.startsWith(prefix));
         res.json(newCart.slice(0, pageSize));
     } else if (req.query.pageSize && !req.query.prefix && !req.query.maxPrice) {
-        const pageSize = Number(req.query.pageSize);
         res.json(cart.slice(0, pageSize));
     } else if (req.query.pageSize && req.query.prefix && req.query.pageSize) {
-        const maxPrice = Number(req.query.maxPrice);
-        const prefix = String(req.query.prefix);
-        const pageSize = Number(req.query.pageSize);
-
         const newCart = cart.filter(item => item.product.startsWith(prefix));
         const newerCart = newCart.filter(item => item.price <= maxPrice);
         res.json(newerCart.slice(0, pageSize));
