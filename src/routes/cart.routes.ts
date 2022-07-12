19bd94cfbbeef7f6691b0b0f394e82739746afdc
Marlee Garcia <marlee.garcia121@gmail.com>
2022-07-11 21:17:38 -0400

    Cleaner queries from notes

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -6,31 +6,52 @@ import { v4 as uuidv4 } from "uuid";
 export const cartRouter = express.Router();
 
 cartRouter.get('/', (req, res) => {
-    const maxPrice = Number(req.query.maxPrice);
-    const prefix = String(req.query.prefix);
-    const pageSize = Number(req.query.pageSize);
 
-    if (req.query.maxPrice && !req.query.prefix && !req.query.pageSize) {
-        res.json(cart.filter(item => item.price <= maxPrice));        
-    } else if (req.query.maxPrice && req.query.prefix && !req.query.pageSize) {
-        const newCart = cart.filter(item => item.product.startsWith(prefix));
-        res.json(newCart.filter(item => item.price <= maxPrice));
-    } else if (req.query.maxPrice && req.query.pageSize && !req.query.prefix) {
-        const newCart = cart.filter(item => item.price <= maxPrice);
-        res.json(newCart.slice(0, pageSize));
-    } else if (req.query.prefix && !req.query.maxPrice && !req.query.pageSize) {
-        res.json(cart.filter(item => item.product.startsWith(prefix)));        
-    } else if (req.query.prefix && req.query.pageSize && !req.query.maxPrice) {
-        const newCart = cart.filter(item => item.product.startsWith(prefix));
-        res.json(newCart.slice(0, pageSize));
-    } else if (req.query.pageSize && !req.query.prefix && !req.query.maxPrice) {
-        res.json(cart.slice(0, pageSize));
-    } else if (req.query.pageSize && req.query.prefix && req.query.pageSize) {
-        const newCart = cart.filter(item => item.product.startsWith(prefix));
-        const newerCart = newCart.filter(item => item.price <= maxPrice);
-        res.json(newerCart.slice(0, pageSize));
-    }
-    res.json(cart);
+        let filtered = cart;
+    
+        if (req.query.maxPrice) {
+            filtered = filtered.filter(item => item.price <= Number(req.query.maxPrice));
+        }
+    
+        if (req.query.prefix) {
+    
+            filtered = filtered.filter(item => item.product.startsWith(String(req.query.prefix)));
+        }
+    
+        if (req.query.pageSize) {
+    
+            filtered = filtered.slice(0, +req.query.pageSize)
+        }
+    
+        res.status(200).json(filtered)
+
+
+
+    // const maxPrice = Number(req.query.maxPrice);
+    // const prefix = String(req.query.prefix);
+    // const pageSize = Number(req.query.pageSize);
+
+    // if (req.query.maxPrice && !req.query.prefix && !req.query.pageSize) {
+    //     res.json(cart.filter(item => item.price <= maxPrice));        
+    // } else if (req.query.maxPrice && req.query.prefix && !req.query.pageSize) {
+    //     const newCart = cart.filter(item => item.product.startsWith(prefix));
+    //     res.json(newCart.filter(item => item.price <= maxPrice));
+    // } else if (req.query.maxPrice && req.query.pageSize && !req.query.prefix) {
+    //     const newCart = cart.filter(item => item.price <= maxPrice);
+    //     res.json(newCart.slice(0, pageSize));
+    // } else if (req.query.prefix && !req.query.maxPrice && !req.query.pageSize) {
+    //     res.json(cart.filter(item => item.product.startsWith(prefix)));        
+    // } else if (req.query.prefix && req.query.pageSize && !req.query.maxPrice) {
+    //     const newCart = cart.filter(item => item.product.startsWith(prefix));
+    //     res.json(newCart.slice(0, pageSize));
+    // } else if (req.query.pageSize && !req.query.prefix && !req.query.maxPrice) {
+    //     res.json(cart.slice(0, pageSize));
+    // } else if (req.query.pageSize && req.query.prefix && req.query.pageSize) {
+    //     const newCart = cart.filter(item => item.product.startsWith(prefix));
+    //     const newerCart = newCart.filter(item => item.price <= maxPrice);
+    //     res.json(newerCart.slice(0, pageSize));
+    // }
+    // res.json(cart);
 });
 
 cartRouter.get('/:id', (req, res) => {
